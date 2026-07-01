f8cf689f3f98c8343eb4ae662e6b8ddb2445c0cc
pavega25 <materializacreative@gmail.com>
2026-07-01 08:13:53 +0200

    asdasd

--- components/cookies/script_boton_flotante_preferencias.js
+++ components/cookies/script_boton_flotante_preferencias.js
@@ -1,6 +1,6 @@
 // Añadir un botón flotante para cambiar preferencias después de aceptar
 const floatingBtn = document.createElement('button');
-floatingBtn.innerHTML = '<img src="assets/images/favicon.png">';
+floatingBtn.innerHTML = '<img src="assets/images/favicon.png"> style="border-radius: 50%"';
 floatingBtn.style.cssText = `
     position: fixed;
     bottom: 20px;
